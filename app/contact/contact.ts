ba04aeaf337b9a482edede30fb897928746fbd14
Terje Barstad Olsen <tebaol@statoil.com>
2016-09-09 13:51:51 +0200

    Improved contact form.

--- app/contact/contact.ts
+++ app/contact/contact.ts
@@ -1,17 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {Contact} from "./contactmodel";
+import {NgForm} from "@angular/forms";
+
 
 @Component({
     selector: 'contact',
     templateUrl: 'app/contact/contact.html',
 })
 export class ContactComponent {
-    contactRequest = new Contact("din@epost.no","Din beskjed");
+    @ViewChild("contactform") form : NgForm;
+    contactRequest = new Contact();
     submitted: boolean;
 
     onSubmit() {
-        console.log("Submitted");
+        console.log("Submitted", this.contactRequest);
         this.submitted = true;
+        this.form.resetForm();
     }
 
 }
--- app/contact/contactmodel.ts
+++ app/contact/contactmodel.ts
@@ -1,4 +1,4 @@
 export class Contact
 {
-    constructor(public sender: string, public message: string) {}
+    constructor(public sender = "", public message = "") {}
 }
